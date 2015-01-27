84bd84a7849fea0fcbc3c6f589bd4597d1531d6b
David Tran <davidt93@uw.edu>
2015-01-27 15:43:58 -0800

    linear regress done

--- RidgeRegression/ridgeregression.py
+++ RidgeRegression/ridgeregression.py
@@ -1,6 +1,8 @@
 import numpy as np
 import scipy.sparse as sp
-from scipy.sparse import coo_matrix, hstack, linalg
+from scipy import linalg
+from scipy.sparse import coo_matrix, hstack
+import math
 # Load a text file of integers:
 y = np.loadtxt("./hw1-data/upvote_labels.txt", dtype=np.int)
 # Load a text file of strings:
@@ -8,20 +10,29 @@ featureNames = open("./hw1-data/upvote_features_100.txt").read().splitlines()
 # Load a csv of floats:
 A = sp.csc_matrix(np.genfromtxt("./hw1-data/upvote_data_100.csv", delimiter=","))
 
+def RMSE(W):
+	sum = W.sum()
+	print sum
+
 bias = []
-for i in range(6000):
+for i in range(5000):
 	bias.append([1])
 B = coo_matrix(bias)
 
-# print hstack([B,B]).todense()
-HFull = hstack([B,A]).todense()
-deleteArr = range(5000,6000)
-H = np.delete(HFull, deleteArr, 0)
-HSparse = sp.csc_matrix(H)
-HTrans = sp.csc_matrix.transpose(HSparse)
+HPart = sp.csc_matrix(A[:5000])
+
+
+HFull = hstack([B,HPart]).todense()
+HTrans = HFull.transpose()
+HTH = HTrans * HFull
+inverse = linalg.pinv(HTH)
 T = sp.csc_matrix.transpose(sp.csc_matrix(y[:5000]))
-HTH = HTrans*(HSparse)
-inverse = sp.linalg.inv(HTH)
-temp = inverse*HTrans
-W = temp*T
+W = inverse * HTrans * T
+
+
+HWt = (HFull * W - T)
+HWtTrans = HWt.transpose()
+residError = HWtTrans * HWt
+RMSE = math.sqrt(residError/5000.0)
+print RMSE
 
