e8b928da565b0771fb87abbdcb090804a84c983e
David Tran <davidt93@uw.edu>
2015-01-27 01:38:07 -0800

    ridge working on it

--- RidgeRegression/ridgeregression.py
+++ RidgeRegression/ridgeregression.py
@@ -1,8 +1,9 @@
 import numpy as np
 import scipy.sparse as sp
 # Load a text file of integers:
-y = np.loadtxt("upvote_labels.txt", dtype=np.int)
+y = np.loadtxt("./hw1-data/upvote_labels.txt", dtype=np.int)
 # Load a text file of strings:
-featureNames = open("upvote_features_100.txt").read().splitlines()
+featureNames = open("./hw1-data/upvote_features_100.txt").read().splitlines()
 # Load a csv of floats:
-A = sp.csc_matrix(np.genfromtxt("upvote_data_100.csv", delimiter=","))
+A = sp.csc_matrix(np.genfromtxt("./hw1-data/upvote_data_100.csv", delimiter=","))
+
