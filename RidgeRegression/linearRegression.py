6b139d891c7b3a6d8b2525b77e1e643b2a2541b1
David Tran <davidt93@uw.edu>
2015-01-27 21:19:19 -0800

    done with ridge

--- RidgeRegression/linearRegression.py
+++ RidgeRegression/linearRegression.py
@@ -10,10 +10,14 @@ featureNames = open("./hw1-data/upvote_features_100.txt").read().splitlines()
 # Load a csv of floats:
 A = sp.csc_matrix(np.genfromtxt("./hw1-data/upvote_data_100.csv", delimiter=","))
 
-bias = []
-for i in range(5000):
-	bias.append([1])
-B = coo_matrix(bias)
+def makeBias(value, length):
+	bias = []
+	for i in range(length):
+		bias.append([value])
+	B = coo_matrix(bias)
+	return B
+
+B = makeBias(1, 5000)
 
 HTrain = sp.csc_matrix(A[:5000])
 HTrainBias = hstack([B,HTrain]).todense()
@@ -30,10 +34,7 @@ residError = HWtTrans * HWt
 RMSE = math.sqrt(residError/5000.0)
 print RMSE
 
-bias = []
-for i in range(1000):
-	bias.append([1])
-B = coo_matrix(bias)
+B = makeBias(1, 1000)
 
 HTest = sp.csc_matrix(A[5000:6000])
 HTestBias = hstack([B,HTest]).todense()
@@ -41,9 +42,6 @@ HTrainTrans = HTestBias.transpose()
 HTH = HTrainTrans * HTestBias
 inverse = linalg.pinv(HTH)
 T = sp.csc_matrix.transpose(sp.csc_matrix(y[5000:6000]))
-
-# print HTestBias.shape
-# print W.shape
 
 HWt = (HTestBias * W - T)
 HWtTrans = HWt.transpose()
