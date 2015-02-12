e5b484d3bca16df2e8a6c8ca004da84ff3893e83
davidt93 <davidt93@uw.edu>
2015-02-12 13:55:54 -0800

    almost done with perceptron

--- Perceptron/hw2-data/start.py
+++ Perceptron/hw2-data/start.py
@@ -4,180 +4,10 @@ from numpy import *
 import matplotlib.pyplot as plt
 
 def main():
-    training_data = genfromtxt('train.txt', delimiter=',')
-    Y_train = training_data[:,0]
-    X_train = training_data[:, 1:]
-    Y_test = genfromtxt('test_label.txt', delimiter=',')
-    X_test = genfromtxt('test.txt', delimiter=',')
-
-    lenData = len(Y_train)
-    featureLen = len(training_data[0]) - 1
-    baisArray = [1] * lenData
-    Weights = [0] * featureLen
-    X_Matrix = matrix(X_train)
-    Y_Matrix = matrix(Y_train)
-    X_Test = matrix(X_test)
-    Y_Test = matrix(Y_test)
-    W_Matrix = transpose(matrix(Weights))
-    W_T = matrix(Weights)
-    X_T = transpose(X_Matrix)
-
     wo = 0.0
     eta = 0.1
     lamb = 0.3
     plotArray = []
-
-    # 2.4.2
-    # 1000 times
-    for t in range(1000):
-	# W not wo
-    	YJMP = getYJMinusP(X_T, W_T, Y_Matrix, wo)
-    	WUpdateXY = (X_T * transpose(YJMP)) / (lenData + 0.0)
-    	W_Matrix = W_Matrix - eta * (lamb * W_Matrix - WUpdateXY)
-   	# bias without something
-    	# wo <= wo + func
-    	wo = wo + eta * sum(YJMP) / (lenData + 0.0)
-	W_T = transpose(W_Matrix)
-	print t,
-	print " \t",
-	logloss = getLogLoss(lamb, W_Matrix, X_T, W_T, wo, lenData, Y_Matrix).item(0)
-	print logloss
-	plotArray.append(logloss)
-    x = range(1000)
-    plt.plot(x, plotArray, linewidth=2.0)
-    plt.show()
-    learnedWeights = W_Matrix
-    CTR = getCTRCorrects(transpose(X_Test), transpose(learnedWeights), Y_test, wo)
-    print "CTR correct guess: ",
-    print CTR
-    print learnedWeights
-
-    # 2.4.3
-    episilon = 0.0005
-    plotArray = []
-    lastLL = None
-    Weights = [0] * featureLen
-    wo = 0.0
-    W_Matrix = transpose(matrix(Weights))
-    W_T = matrix(Weights)
-    for t in range(1000):
-	# W not wo
-    	YJMP = getYJMinusP(X_T, W_T, Y_Matrix, wo)
-    	WUpdateXY = (X_T * transpose(YJMP)) / (lenData + 0.0)
-    	W_Matrix = W_Matrix - eta * (lamb * W_Matrix - WUpdateXY)
-   	# bias without something
-    	# wo <= wo + func
-    	wo = wo + eta * sum(YJMP) / (lenData + 0.0)
-	W_T = transpose(W_Matrix)
-	print t,
-	print " \t",
-	logloss = getLogLoss(lamb, W_Matrix, X_T, W_T, wo, lenData, Y_Matrix).item(0)
-	print logloss
-	if lastLL == None:
-		lastLL = logloss
-	elif abs(lastLL - logloss) < episilon:
-		break
-	lastLL = logloss	
-	plotArray.append(logloss)
-    x = range(len(plotArray))
-    plt.plot(x, plotArray, linewidth=2.0)
-    plt.show()
-    learnedWeights = W_Matrix
-    CTR = getCTRCorrects(transpose(X_Test), transpose(learnedWeights), Y_test, wo)
-    print "CTR correct guess: ",
-    print CTR
-    print learnedWeights
-
-    # 2.4.4
-    lamb = 0
-    episilon = 0.0000
-    plotArray = []
-    lastLL = None
-    Weights = [0] * featureLen
-    wo = 0.0
-    W_Matrix = transpose(matrix(Weights))
-    W_T = matrix(Weights)
-    for t in range(1000):
-	# W not wo
-    	YJMP = getYJMinusP(X_T, W_T, Y_Matrix, wo)
-    	WUpdateXY = (X_T * transpose(YJMP)) / (lenData + 0.0)
-    	W_Matrix = W_Matrix - eta * (lamb * W_Matrix - WUpdateXY)
-   	# bias without something
-    	# wo <= wo + func
-    	wo = wo + eta * sum(YJMP) / (lenData + 0.0)
-	W_T = transpose(W_Matrix)
-	#print t,
-	#print " \t",
-	logloss = getLogLoss(lamb, W_Matrix, X_T, W_T, wo, lenData, Y_Matrix).item(0)
-	#print logloss
-	if lastLL == None:
-		lastLL = logloss
-	elif abs(lastLL - logloss) < episilon:
-		break
-	lastLL = logloss	
-	plotArray.append(logloss)
-    learnedWeights = W_Matrix
-    print "LLNorm: ",
-    print getLLNorm(learnedWeights)
-    
-    lamb = 0.3
-    episilon = 0.0000
-    plotArray = []
-    lastLL = None
-    Weights = [0] * featureLen
-    wo = 0.0
-    W_Matrix = transpose(matrix(Weights))
-    W_T = matrix(Weights)
-    for t in range(1000):
-	# W not wo
-    	YJMP = getYJMinusP(X_T, W_T, Y_Matrix, wo)
-    	WUpdateXY = (X_T * transpose(YJMP)) / (lenData + 0.0)
-    	W_Matrix = W_Matrix - eta * (lamb * W_Matrix - WUpdateXY)
-   	# bias without something
-    	# wo <= wo + func
-    	wo = wo + eta * sum(YJMP) / (lenData + 0.0)
-	W_T = transpose(W_Matrix)
-	#print t,
-	#print " \t",
-	logloss = getLogLoss(lamb, W_Matrix, X_T, W_T, wo, lenData, Y_Matrix).item(0)
-	#print logloss
-	if lastLL == None:
-		lastLL = logloss
-	elif abs(lastLL - logloss) < episilon:
-		break
-	lastLL = logloss	
-	plotArray.append(logloss)
-    learnedWeights = W_Matrix
-    print "LLNorm: ",
-    print getLLNorm(learnedWeights)
-
-    #2.5.1
-    for t in range(5000):
-	# W not wo
-    	YJMP = getYJMinusP(X_T, W_T, Y_Matrix, wo)
-    	WUpdateXY = (X_T * transpose(YJMP)) / (lenData + 0.0)
-    	W_Matrix = W_Matrix - eta * (lamb * W_Matrix - WUpdateXY)
-   	# bias without something
-    	# wo <= wo + func
-    	wo = wo + eta * sum(YJMP) / (lenData + 0.0)
-	W_T = transpose(W_Matrix)
-	#print t,
-	#print " \t",
-	logloss = getLogLoss(lamb, W_Matrix, X_T, W_T, wo, lenData, Y_Matrix).item(0)
-	#print logloss
-	plotArray.append(logloss)
-    learnedWeights = W_Matrix
-    answers = recallPrecision(transpose(X_Test), transpose(learnedWeights), Y_test, wo)
-    print "Recall1: ",
-    print answers[0]
-    print "Precision1: ", 
-    print answers[1]
-    print "Recall0: ",
-    print answers[2]
-    print "Precision0: ", 
-    print answers[3]
-
-    #2.5.2
 
     training_data = genfromtxt('oversampled_train.txt', delimiter=',')
     Y_train = training_data[:,0]
@@ -189,39 +19,41 @@ def main():
     featureLen = len(training_data[0]) - 1
     baisArray = [1] * lenData
     Weights = [0] * featureLen
-    X_Matrix = matrix(X_train)
-    Y_Matrix = matrix(Y_train)
-    X_Test = matrix(X_test)
-    Y_Test = matrix(Y_test)
-    W_Matrix = transpose(matrix(Weights))
-    W_T = matrix(Weights)
-    X_T = transpose(X_Matrix)
+    W_Matrix = matrix(Weights) 
+    X_Matrix_Trans = transpose(matrix(X_train))
 
-
+    YiNeg = [-1] * featureLen
+    YiPos = [1] * featureLen
+    
     for t in range(5000):
-	# W not wo
-    	YJMP = getYJMinusP(X_T, W_T, Y_Matrix, wo)
-    	WUpdateXY = (X_T * transpose(YJMP)) / (lenData + 0.0)
-    	W_Matrix = W_Matrix - eta * (lamb * W_Matrix - WUpdateXY)
-   	# bias without something
-    	# wo <= wo + func
-    	wo = wo + eta * sum(YJMP) / (lenData + 0.0)
-	W_T = transpose(W_Matrix)
-	#print t,
-	#print " \t",
-	logloss = getLogLoss(lamb, W_Matrix, X_T, W_T, wo, lenData, Y_Matrix).item(0)
-	#print logloss
-	plotArray.append(logloss)
-    learnedWeights = W_Matrix
-    answers = recallPrecision(transpose(X_Test), transpose(learnedWeights), Y_test, wo)
-    print "Recall1: ",
-    print answers[0]
-    print "Precision1: ", 
-    print answers[1]
-    print "Recall0: ",
-    print answers[2]
-    print "Precision0: ", 
-    print answers[3]
+        countCorrect = 0
+	for i in range(lenData):
+		Y = sign(W_Matrix, X_train[i], wo)
+		if Y != Y_train[i]:
+			if Y_train[i] == 1:
+				W_Matrix = W_Matrix + YiXi(YiPos, X_train[i])
+				wo = wo + Y_train[i]
+			else:
+				W_Matrix = W_Matrix + YiXi(YiNeg, X_train[i])			
+				wo = wo + (-1)
+		else:
+			countCorrect += 1
+	print "t: ",
+	print t,
+	print "correct: ",
+	print countCorrect
+
+
+def sign(W_T, X_T, wo):
+	X_Matrix = transpose(matrix(X_T))
+	if (W_T * X_Matrix).item(0) + wo > 0:
+		return 1
+	else:
+		return 0
+
+# returns Xi * constant Yi
+def YiXi(Yi, Xi):
+	return dot(Yi, Xi)
 
 # from slides
 def getLogLoss(lamb, W_Matrix, X_T, W_T, wo, lenData, Y_Matrix):
@@ -292,12 +124,6 @@ def recallPrecision(X_T, W_T, Y_test, wo):
 
 def getLLNorm(W_Matrix):
 	return transpose(W_Matrix) * W_Matrix
-###############################################################################################################
-####################################################Perceptron#################################################
-###############################################################################################################
-
-def perceptron():
-	return 0
 
 
 
